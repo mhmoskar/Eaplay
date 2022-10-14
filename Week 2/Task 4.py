65a61b7357f16e915175128764cb57cce966635e
Oskar <112126517+mhmoskar@users.noreply.github.com>
2022-10-14 18:16:34 +0100

    Update Task 4.py

--- Week 2/Task 4.py
+++ Week 2/Task 4.py
@@ -1,12 +1,12 @@
-def Sq(X):
-    return X * X
-A = 9
-print(A, "squared",Sq(A))
+#driving test problem
+#Subroutine to output pass or fail for driving test
 
-#String = line 4 
-#Subroutine = line 2
-#Squared 9 = line 2
-#Comma in line 4 = Used to split the string to put the variables in as placing A into "" would output "A squared Sq(A)" which is not what we want.
-#cmd print = writes it in the console.
-#Double quote "" = Used to output squared into the console as it is not a variable but a string. 
-# Why is A = X = A = 9 thefore in the print it uses the variable of Sq() and replaces X with A which is 9 and then returns the answer of A squared.
+def PassFail(MinorFaults):
+  if MinorFaults < 16:
+    return "pass"
+  else:
+    return "fail"
+
+#main program
+print(PassFail(16))
+    
